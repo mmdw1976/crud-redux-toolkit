59e2f0cfbd65a2278a925e3ae7e0610ffbd53ee8
Mario <mmdw1976@gmail.com>
2021-10-05 12:54:15 +0200

    setup userSlice reducer/action logic

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -21,7 +21,45 @@ const userSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
-        
+        addUser: (state, action) => {
+            return {
+                ...state,
+                users: [...state.users, action.payload]
+            }
+        },
+        editUser: (state, action) => {
+            return {
+                ...state,
+                currentUser: action.payload,
+                edit: true
+            }
+        },
+        cancelEdit: state => {
+            return {
+                ...state,
+                currentUser: null,
+                edit: false
+            }
+        },
+        updateUser: (state, action) => {
+            return {
+                ...state,
+                users: state.users.map(user => {
+                    if (user.id === action.payload.id) {
+                        return action.payload;
+                    }
+                    return user;
+                }),
+                currentUser: null,
+                edit: false
+            }
+        },
+        deleteUser: (state, action) => {
+            return {
+                ...state,
+                users: state.users.filter(user => user.id !== action.payload);
+            }
+        }
     }
 })
 
